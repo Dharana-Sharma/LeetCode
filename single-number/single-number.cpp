226781551aa3cf076c131abe2636cee0fa1b8278
Dharana Sharma <88919299+Dharana-Sharma@users.noreply.github.com>
2022-02-02 13:46:57 +0530

    Time: 16 ms, Memory: 16.8 MB - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+    int single = 0;
+    for( int i = 0; i < nums.size(); i++ )
+    {
+        single = single xor nums[i];
+    }
+    return single;
+}
+};
