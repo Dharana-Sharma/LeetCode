c66427078037b40a7ae63451ff7497fddfac734c
Dharana Sharma <88919299+Dharana-Sharma@users.noreply.github.com>
2022-02-02 13:51:44 +0530

    Time: 31 ms, Memory: 23.3 MB - LeetHub

--- reverse-string/reverse-string.cpp
+++ reverse-string/reverse-string.cpp
@@ -5,9 +5,9 @@ public:
     int n = s.size();
     for( int i = 0; i < n/2; i++ )
     {
-        char temp = s[i];
+        char temp = s[i];         // or use swap(s[i],s[n-i-1]);
         s[i] = s[n-1-i];
-        s[n-1-i] = temp;
+        s[n-1-i] = temp;                                                                                                           
     }
 }
 };
